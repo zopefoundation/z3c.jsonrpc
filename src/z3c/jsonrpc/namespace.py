995179253cf3ddc49086f3e861564923d5f8403e
Stephan Richter <stephan.richter@gmail.com>
2008-01-24 22:46:42 +0000

    - Bug: The skin code relied on un-released API that was actually later   reverted.

--- src/z3c/jsonrpc/namespace.py
+++ src/z3c/jsonrpc/namespace.py
@@ -1,12 +1,20 @@
-# Make a package.
-
 import zope.component
+import zope.interface
 import zope.traversing.namespace
 from zope.component.interfaces import ComponentLookupError
-from zope.publisher.http import applySkin
 from zope.traversing.interfaces import TraversalError
 
 from z3c.jsonrpc import interfaces
+
+
+def applySkin(request, skin, skin_type):
+       """Change the presentation skin for this request."""
+       # Remove all existing skin declarations (commonly the default skin).
+       ifaces = [iface for iface in zope.interface.directlyProvidedBy(request)
+                 if not skin_type.providedBy(iface)]
+       # Add the new skin.
+       ifaces.append(skin)
+       zope.interface.directlyProvides(request, *ifaces)
 
 
 class skin(zope.traversing.namespace.skin):
