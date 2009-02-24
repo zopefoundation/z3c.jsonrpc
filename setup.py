0866fd2d90b3118e9fd86eaf689b080cc6f78328
Roger Ineichen <roger@projekt01.ch>
2009-02-24 03:52:49 +0000

    cleanup doc generation

--- setup.py
+++ setup.py
@@ -20,8 +20,7 @@ import xml.sax.saxutils
 from setuptools import setup, find_packages
 
 def read(*rnames):
-    text = open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-    return xml.sax.saxutils.escape(text)
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup (
     name='z3c.jsonrpc',
